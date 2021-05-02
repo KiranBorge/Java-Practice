cb4cf168a2da1d8699ee43fd9798a0c19fd88332
KiranBorge <83537071+KiranBorge@users.noreply.github.com>
2021-05-02 20:09:57 +0530

    For Loop Added

--- Test.java
+++ Test.java
@@ -3,5 +3,9 @@ class Test
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
+    for(int i=0;i<5;i++)
+    {
+       System.out.println("Hello!");
+    }
   }
 }
